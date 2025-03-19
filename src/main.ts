85e351681376287d53cfb61b8301eeb400eb7bee
blackdoll-loki <helpmeloki7@gmail.com>
2025-03-19 20:27:35 +0200

    add CORS url

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'https://test-task5-front.vercel.app', // URL вашого фронтенду на Vercel
+    methods: 'GET,POST,PATCH,DELETE',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
