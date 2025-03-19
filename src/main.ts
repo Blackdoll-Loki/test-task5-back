a919a760c1fddfad57e08f78904b2e73b1e1b672
blackdoll-loki <helpmeloki7@gmail.com>
2025-03-19 21:30:13 +0200

    try to find DATABAE_URL

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,11 @@ async function bootstrap() {
     methods: 'GET,POST,PATCH,DELETE',
     credentials: true,
   };
+
+  const configService = app.get(ConfigService);
+  console.log('DATABASE_URL:', configService.get('DATABASE_URL'));
+  console.log('process.env.DATABASE_URL:', process.env.DATABASE_URL);
+
 
   app.enableCors(corsOptions);
 
